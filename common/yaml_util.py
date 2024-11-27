de9ae0696d4f246c1db9e42672461452e3806814
jiawenlin1 <ljw1033@126.com>
2024-11-27 15:52:50 +0800

    DEMO

--- common/yaml_util.py
+++ common/yaml_util.py
@@ -4,7 +4,11 @@ import os
 
 #获取项目路径
 def get_object_path():
-    return os.path.abspath(os.getcwd().split('common')[0])
+    # 获取当前脚本文件所在的绝对路径
+    script_path = os.path.abspath(__file__)
+    print("【get_object_path()】方法返回：" + os.path.dirname(script_path).split('common')[0])
+    # 返回该路径的父目录，即工程路径
+    return os.path.dirname(script_path).split('common')[0]
 
 #读取config.yml文件
 def read_config_file(one_node,two_node):
@@ -16,6 +20,7 @@ def read_config_file(one_node,two_node):
 def read_extract_file(one_node):
     with open(get_object_path()+"\\extract.yml",encoding='utf-8') as f:
         value = yaml.load(stream=f, Loader=yaml.FullLoader)
+        print("value is ",value)
         return value[one_node]
 
 #写入extract.yml文件a
@@ -28,6 +33,6 @@ def clear_extract_file():
     with open(get_object_path()+"\\extract.yml",encoding='utf-8',mode='w') as f:
         f.truncate()
 
-if __name__ == '__main__':
-    print(read_config_file('base','base_url'))
-    print(get_object_path())
+# if __name__ == '__main__':
+#     print(read_config_file('base','base_url'))
+#     print(get_object_path())
