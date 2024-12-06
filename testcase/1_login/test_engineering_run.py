fd4c0f6b1fa12b4f7bd1829f96af9e2f94194c77
jiawenlin1 <ljw1033@126.com>
2024-12-06 15:17:03 +0800

    增加了获取密钥API

--- testcase/1_login/test_engineering_run.py
+++ testcase/1_login/test_engineering_run.py
@@ -8,8 +8,16 @@ from common.requests_util import RequestUtil
 @allure.epic("IN2.0")
 @allure.feature("demo")
 class TestCreat():
+    @allure.story("接口名称：获取key")
+    @pytest.mark.parametrize('caseinfo', read_testcase_file('/testcase/1_login/get_encryptkey.yml'))
+    def test_get_rencryptkey(self, caseinfo):
+        allure.dynamic.title(caseinfo['name'])
+        allure.dynamic.description(caseinfo['name'])
+        RequestUtil().analysis_yaml(caseinfo)
+        time.sleep(2)
+
     @allure.story("接口名称：登录")
-    @pytest.mark.parametrize('caseinfo', read_testcase_file('/testcase/1_EngineeringManagement/creat_folder.yml'))
+    @pytest.mark.parametrize('caseinfo', read_testcase_file('/testcase/1_login/login.yml'))
     def test_login(self, caseinfo):
         allure.dynamic.title(caseinfo['name'])
         allure.dynamic.description(caseinfo['name'])
@@ -17,18 +25,11 @@ class TestCreat():
         time.sleep(2)
 
 
-    @allure.story("接口名称：获取用户信息1")
-    @pytest.mark.parametrize('caseinfo', read_testcase_file('/testcase/1_EngineeringManagement/data_backup.yml'))
-    def test_getinfo(self, caseinfo):
+    @allure.story("接口名称：获取用户信息12")
+    @pytest.mark.parametrize('caseinfo', read_testcase_file('/testcase/1_login/get_user_info.yml'))
+    def test_get_user_info(self, caseinfo):
         allure.dynamic.title(caseinfo['name'])
         allure.dynamic.description(caseinfo['name'])
         RequestUtil().analysis_yaml(caseinfo)
         time.sleep(2)
 
-    @allure.story("接口名称：重命名工程1")
-    @pytest.mark.parametrize('caseinfo', read_testcase_file('/testcase/1_EngineeringManagement/data_backup.yml'))
-    def test_getinfo(self, caseinfo):
-        allure.dynamic.title(caseinfo['name'])
-        allure.dynamic.description(caseinfo['name'])
-        RequestUtil().analysis_yaml(caseinfo)
-        time.sleep(2)
